7dd5504dc7f332ebb75cb36141b7b95157b65bf7
Elio <nguyen2332004@gmail.com>
2025-03-02 11:36:01 +0700

    fix: update loading component background image for consistency

--- public/loading-component.php
+++ public/loading-component.php
@@ -57,7 +57,7 @@ $loading_text = isset($loading_text) ? $loading_text : "ENTERING WILDLIFE HAVEN"
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.05'/%3E%3C/svg%3E");
+  background-image: url("/Applications/XAMPP/xamppfiles/htdocs/Wildlife/public/img/image.png");
   pointer-events: none;
   opacity: 0.4;
 }
